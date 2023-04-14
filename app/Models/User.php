e89baad9f235daeb40a64c0e78ab857a5c562e80
vincentestenzo113 <vincentestenzo1@gmail.com>
2023-04-14 23:46:43 +0800

    changes6

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 class User extends Model{
 protected $table = 'customers';
+public $timestamps = false;
 // column sa table
 protected $fillable = [
 'customer_first_name', 'customer_last_name','customer_phone_number'
